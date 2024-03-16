9bf225e5fc31123ce9df0223eb6c05a39391fc2b
Stathis Koukouvinos <stathis.koukouvinos@gmail.com>
2024-03-16 17:41:21 +0200

    test: create UserActionMovingAverageJobTest together with helper test classes TestSourceFunction and TestSinkFunction

---
+++ src/test/scala/co/example/utils/TestSinkFunction.scala
@@ -0,0 +1,23 @@
+package co.example.utils
+
+import org.apache.flink.api.common.JobExecutionResult
+import org.apache.flink.api.common.accumulators.ListAccumulator
+import org.apache.flink.configuration.Configuration
+import org.apache.flink.streaming.api.functions.sink.{RichSinkFunction, SinkFunction}
+
+class TestSinkFunction[T] extends RichSinkFunction[T] {
+
+  private val accName: String = "results"
+
+  override def open(parameters: Configuration): Unit =
+    getRuntimeContext.addAccumulator(accName, new ListAccumulator[T])
+
+  override def invoke(value: T, context: SinkFunction.Context): Unit =
+    getRuntimeContext.getAccumulator(accName).add(value)
+
+  def getResults(jobExecutionResult: JobExecutionResult): java.util.List[T] = {
+    //import scala.collection.JavaConverters._
+    val l: java.util.List[T] = jobExecutionResult.getAccumulatorResult(accName)
+    l//.asScala.toList
+  }
+}
---
+++ src/test/scala/co/example/utils/TestSourceFunction.scala
@@ -0,0 +1,16 @@
+package co.example.utils
+
+import org.apache.flink.streaming.api.functions.source.SourceFunction
+
+class TestSourceFunction[T](events: List[T]) extends SourceFunction[T] {
+
+  override def run(ctx: SourceFunction.SourceContext[T]): Unit =
+    for (e <- events) {
+      ctx.collect(e)
+      Thread.sleep(1000)
+    }
+
+  override def cancel(): Unit = {
+    // ignore cancel, finite anyway
+  }
+}
