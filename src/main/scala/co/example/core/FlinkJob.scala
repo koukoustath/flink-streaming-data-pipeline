8d2d6acd55a39494ef64844ac10166e5d7e66e5a
Stathis Koukouvinos <stathis.koukouvinos@gmail.com>
2024-03-16 17:37:51 +0200

    test: change return type of run method in FlinkJob for testing

--- src/main/scala/co/example/core/FlinkJob.scala
+++ src/main/scala/co/example/core/FlinkJob.scala
@@ -1,12 +1,13 @@
 package co.example.core
 
+import org.apache.flink.api.common.JobExecutionResult
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 
 trait FlinkJob {
 
   def job(executionEnvironment: StreamExecutionEnvironment): Unit
 
-  def run(jobName: String): Unit = {
+  def run(jobName: String): JobExecutionResult = {
     val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
     job(env)
     env.execute(jobName)
